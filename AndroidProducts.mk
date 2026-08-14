CalyxOS/device_motorola_hawao

Initial CalyxOS makefile
Change-Id: Ieefcb32c1619d7623f788bc910d16811b881b8be

AndroidProducts.mk
@@ -5,6 +5,7 @@
 #
 
 PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/calyx_hawao.mk \
     $(LOCAL_DIR)/lineage_hawao.mk
 
 COMMON_LUNCH_CHOICES := \

moto-sm8450/android_device_motorola_avatrn

Change-Id: I6dfcc9b2b5b686df8d2bbe4183bb687777ab576e

AndroidProducts.mk
@@ -15,4 +15,5 @@
 #
 
 PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/calyx_avatrn.mk \
     $(LOCAL_DIR)/lineage_avatrn.mk